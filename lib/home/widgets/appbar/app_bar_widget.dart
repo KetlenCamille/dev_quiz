a74040195fe48e1dc5814fb79481aa0235ec6162
KetlenCamille <ketlen.rieper@gmail.com>
2021-04-19 23:44:50 -0300

    Day 01 - Creating AppBar and ScoreCard

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,51 @@
+import 'package:dev_quiz/core/app_gradients.dart';
+import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget() 
+    : super(
+      preferredSize: Size.fromHeight(250), 
+      child: Container(
+        height: 250,
+        child: Stack(
+          children: [
+            Container(
+              height: 161,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              width: double.maxFinite,
+              decoration: BoxDecoration(gradient: AppGradients.linear),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text.rich(TextSpan(
+                    text: "Hello, ", 
+                    style: AppTextStyles.title,
+                    children:[
+                      TextSpan(
+                      text: "Ketlen Rieper",
+                      style: AppTextStyles.titleBold,
+                      )
+                    ])
+                  ),
+                  Container(
+                    width: 58,
+                    height: 58,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(image: NetworkImage("https://avatars.githubusercontent.com/u/39053868?v=4"))
+                    )
+                  )
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment(0.0, 1.0),
+              child: ScoreCardWidget()
+            ),
+          ],
+        )
+      ),
+    );
+}
---
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -0,0 +1,44 @@
+import 'package:dev_quiz/core/app_colors.dart';
+import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/home/widgets/chart/chart_widget.dart';
+import 'package:flutter/material.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+  const ScoreCardWidget({Key ? key}) : super(key : key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Container(
+        height: 136,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(15)
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: ChartWidget(),
+            ),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: Column(
+                  mainAxisAlignment : MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Let's start", style: AppTextStyles.heading),
+                    Text("Complete challenges and advance knowledge", style: AppTextStyles.body),
+                  ],
+                ),
+              ),
+            )
+          ],
+        )
+      ),
+    );
+  }
+}
