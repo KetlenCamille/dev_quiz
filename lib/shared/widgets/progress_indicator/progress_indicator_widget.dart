cbcc200765d228d0c167ba7cf46d02f2270c5d84
KetlenCamille <ketlen.rieper@gmail.com>
2021-04-21 01:42:30 -0300

    Day 02 - Creating ProgressIndicator and ChallengePage

---
+++ lib/shared/widgets/progress_indicator/progress_indicator_widget.dart
@@ -0,0 +1,16 @@
+import 'package:dev_quiz/core/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class ProgressIndicatorWidget extends StatelessWidget {
+  final double value;
+  const ProgressIndicatorWidget ({Key ? key, required this.value}) : super(key : key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LinearProgressIndicator(
+      value: 0.3,
+      backgroundColor: AppColors.chartSecondary,
+      valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary)
+    );
+  }
+}
