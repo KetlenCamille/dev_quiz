a2c41a542f22ce3f8a11685364234d4b32d3be29
KetlenCamille <ketlen.rieper@gmail.com>
2021-04-21 01:16:06 -0300

    Day 02 - Creating QuizCard

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,5 +1,5 @@
 import 'package:dev_quiz/home/home_page.dart';
-import 'package:dev_quiz/splash/splash_page.dart';
+//import 'package:dev_quiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:dev_quiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:dev_quiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:dev_quiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,7 +13,41 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget()
+      appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 24
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButtonWidget(label: "Easy"),
+                LevelButtonWidget(label: "Medium"),
+                LevelButtonWidget(label: "Hard"),
+                LevelButtonWidget(label: "Expert")
+              ],
+            ),
+            SizedBox(
+              height: 24
+            ),
+            Expanded(
+              child: GridView.count(
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                crossAxisCount: 2,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                ],
+              ),
+            )            
+          ],
+        ),
+      )
     );
   }
 }
