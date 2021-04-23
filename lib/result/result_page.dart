c27d7a719a454bcb8da5bfb9982feb04c3859832
KetlenCamille <ketlen.rieper@gmail.com>
2021-04-23 18:27:15 -0300

    Day 05 - Sharing results on social medias

--- lib/result/result_page.dart
+++ lib/result/result_page.dart
@@ -1,6 +1,7 @@
 import 'package:dev_quiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:dev_quiz/core/core.dart';
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
 class ResultPage extends StatelessWidget{
   final String title;
@@ -53,7 +54,9 @@ class ResultPage extends StatelessWidget{
                         padding: const EdgeInsets.symmetric(horizontal: 68),
                         child: NextButtonWidget.purple(
                           label: "Share",
-                          onTap: (){}
+                          onTap: (){
+                            Share.share('DevQuiz NLW5 - Flutter: Resultado do Quiz $title\nObtive: ${result/length}% de aproveitamento!');
+                          }
                         ),
                       ),
                     ),
