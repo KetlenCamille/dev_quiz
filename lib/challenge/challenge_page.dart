d7e587b9e69c797c5c52d1f4a48240b588b7209c
KetlenCamille <ketlen.rieper@gmail.com>
2021-04-22 21:48:05 -0300

    Day 04 - Creating NextButton and redirect widgets

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,3 +1,4 @@
+import 'package:dev_quiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:dev_quiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:dev_quiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:flutter/material.dart';
@@ -13,10 +14,35 @@ class _ChallengePageState extends State<ChallengePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60),
-        child: SafeArea(top: true, child: QuestionIndicatorWidget()),
+        preferredSize: Size.fromHeight(86),
+        child: SafeArea(top: true, 
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              BackButton(),
+              QuestionIndicatorWidget(),
+            ],
+          )
+        ),
       ),
       body: QuizWidget(title: "O que o Flutter faz em sua totalidade?"),
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: NextButtonWidget.white(label: "Easy", onTap: (){})
+              ),
+              SizedBox(width: 7),
+              Expanded(
+                child: NextButtonWidget.green(label: "Confirm", onTap: (){})
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:dev_quiz/challenge/challenge_page.dart';
 import 'package:dev_quiz/core/app_colors.dart';
 import 'package:dev_quiz/home/widgets/appbar/app_bar_widget.dart';
 import 'package:dev_quiz/home/widgets/level_button/level_button_widget.dart';
@@ -57,6 +58,9 @@ class _HomePageState extends State<HomePage> {
                   children: controller.quizzes!.map((e) => QuizCardWidget(
                     title: e.title, 
                     completed: "${e.questionAwnsered}/${e.questions.length}",
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => ChallengePage()));
+                    },
                     percent: e.questionAwnsered / e.questions.length
                     )).toList()
                 ),
